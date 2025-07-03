38b64ee9210e843489572242daab4371db138681
Nick Goble <nick.goble@dominodatalab.com>
2025-07-03 16:14:07 +0000

    testing

--- flows/sqrt.py
+++ flows/sqrt.py
@@ -5,7 +5,7 @@ value = Path("/workflow/inputs/value").read_text()
 print('my value here should be a string')
 print(value)
 # Calculate square root
-sqrt = int(22) ** 0.5
+sqrt = int(232) ** 0.5
 print(f"The square root of {value} is {sqrt}")
 
 # Write output
