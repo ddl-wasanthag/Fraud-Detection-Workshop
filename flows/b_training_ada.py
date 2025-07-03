34dfdc437dcfc8473135ffa8ccc6502a8f9870b8
Nick Goble <nick.goble@dominodatalab.com>
2025-07-03 19:04:46 +0000

    sdf

--- flows/b_training_ada.py
+++ flows/b_training_ada.py
@@ -1,24 +1,13 @@
 from pathlib import Path
 import pandas as pd
 
-preprocessed_df_path = Path("/workflow/inputs/preprocessed_df_path").read_text()
-
-print(preprocessed_df_path)
-
-test_df = pd.DataFrame({
-    "transaction_id": [1, 2, 3],
-    "amount": [100.0, 200.0, 300.0],
-    "is_fraud": [0, 0, 1]
-})
-
-# Write output
-Path("/workflow/outputs/results_df").write_text(str(test_df))
-
 from flows.generic_trainer import train_fraud
 from helpers.domino_short_id import domino_short_id
 from sklearn.ensemble import AdaBoostClassifier
 
-def train_fraud_ada(random_state=None):
+
+def train_fraud_ada(clean_filepath, random_state=None):
+    model_name = "AdaBoost"
     model_obj = AdaBoostClassifier(
         n_estimators=10,
         learning_rate=0.1,
@@ -26,14 +15,19 @@ def train_fraud_ada(random_state=None):
         random_state=random_state
     )
 
-    model_name = "AdaBoost"
-    clean_filename = 'preprocessing_processed_cc_transactions.csv'
     experiment_name = f"CC Fraud Classifier Training {domino_short_id()}"
 
-    res = train_fraud(model_obj, model_name, clean_filename, experiment_name)
+    res = train_fraud(model_obj, model_name, clean_filepath, experiment_name)
 
     Path("/workflow/outputs/results_df").write_text(res)
     return "train_fraud_ada results"
 
-if __name__ == "__main__":
-    train_fraud_ada()
+preprocessed_df_path = Path("/workflow/inputs/preprocessed_df_path").read_text()
+
+print('pp path', preprocessed_df_path)
+
+res = train_fraud_ada(preprocessed_df_path)
+
+# Write output
+Path("/workflow/outputs/results_df").write_text(res)
+
