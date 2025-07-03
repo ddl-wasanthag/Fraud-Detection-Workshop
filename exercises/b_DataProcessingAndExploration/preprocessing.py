a719086478cb460a75ea64f000c2b62b4e82f7f1
Nick Goble <nick.goble@dominodatalab.com>
2025-07-03 22:10:00 +0000

    sadf

--- exercises/b_DataProcessingAndExploration/preprocessing.py
+++ exercises/b_DataProcessingAndExploration/preprocessing.py
@@ -309,7 +309,7 @@ def run_data_ingestion_and_processing(raw_filename: str, clean_filename: str,
     
     with mlflow.start_run(run_name="Preprocessing Pipeline") as run:
         # Load and clean data
-        df = preprocessor.load_data(raw_filename).head(100)
+        df = preprocessor.load_data(raw_filename).head(102)
         df_clean, cleaning_stats = preprocessor.clean_data(df)
         
         # Prepare features
