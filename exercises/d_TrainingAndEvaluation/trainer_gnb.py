c9806dbf437f8db27ac0416dc1471b6339d6c135
ddl-nick-goble <nick.goble@dominodatalab.com>
2025-07-09 18:19:55 -0400

    Update trainer_gnb.py

--- exercises/d_TrainingAndEvaluation/trainer_gnb.py
+++ exercises/d_TrainingAndEvaluation/trainer_gnb.py
@@ -2,12 +2,13 @@
 from pathlib import Path
 from sklearn.naive_bayes import GaussianNB
 from exercises.d_TrainingAndEvaluation.generic_trainer import train_fraud
-from exercises.1isjdofijwoiej
 
-# Load DataFrame from dataset
+# Load DataFrame from dataset 
 try:
-    transformed_df_filename = Path("/workflow/inputs/transformed_filename").read_text().strip()
-except FileNotFoundError:
+    transformed_df_filename = Path("/workflow/inputs/transformed_filepath").read_text().strip()
+    print('using workflow input: transformed_filepath', transformed_df_filename)
+except FileNotFoundError as e:
+    print('file not found error', e)
     transformed_df_filename = 'transformed_cc_transactions.csv'
 
 model_name = 'GaussianNB'
