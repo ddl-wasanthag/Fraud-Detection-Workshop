a0727f20e66c4c125eef69373cd88ed6a98c75a3
Nick Goble <nick.goble@dominodatalab.com>
2025-07-10 07:47:25 -0700

    Pushed from Domino: https://se-demo.domino.tech/workspace/dmennell/Fraud-Detection-Workshop?executionId=686eeb242cb8c91efbd7ab00

--- exercises/d_TrainingAndEvaluation/workflow.py
+++ exercises/d_TrainingAndEvaluation/workflow.py
@@ -1,11 +1,10 @@
-from flytekit import workflow
+from flytekit import workflow, task
 from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask
 
 
 @workflow
 def simple_math_workflow() -> float:
 
-    
     # Create first task
     add_task = DominoJobTask(
         name='Add numbers',
@@ -28,54 +27,85 @@ def simple_math_workflow() -> float:
 
     return sqrt
 
-# # Fixed credit card workflow
-# @task
-# def preprocessing_task() -> str:
-#     return DominoJobTask(
-#         name='Feature engineering and preprocessing',
-#         domino_job_config=DominoJobConfig(Command="python exercises/DataEngineering/feature_engineering.py"),
-#         inputs={},
-#         outputs={'transformed_filename': str},
-#         use_latest=True,
-#         cache=False
-#     )()
+# Fixed credit card workflow
+@task
+def preprocessing_task() -> str:
+    return DominoJobTask(
+        name='Feature engineering and preprocessing',
+        domino_job_config=DominoJobConfig(Command="python exercises/c_DataEngineering/data_engineering.py"),
+        inputs={},
+        outputs={'transformed_filepath': str},
+        use_latest=True,
+        cache=False
+    )()
 
-# @task
-# def ada_training_task(transformed_filename: str) -> str:
-#     return DominoJobTask(
-#         name='Train AdaBoost classifier',
-#         domino_job_config=DominoJobConfig(Command="python exercises/trainer_ada.py"),
-#         inputs={'transformed_filename': str},
-#         outputs={'results': str},
-#         use_latest=True
-#     )(transformed_filename=transformed_filename)
+@task
+def ada_training_task(transformed_filepath: str) -> str:
+    return DominoJobTask(
+        name='Train AdaBoost classifier',
+        domino_job_config=DominoJobConfig(Command="python exercises/trainer_ada.py"),
+        inputs={'transformed_filepath': str},
+        outputs={'results': str},
+        use_latest=True
+    )(transformed_filepath=transformed_filepath)
 
-# @task
-# def gnb_training_task(transformed_filename: str) -> str:
-#     return DominoJobTask(
-#         name='Train GaussianNB classifier',
-#         domino_job_config=DominoJobConfig(Command="python exercises/trainer_gnb.py"),
-#         inputs={'transformed_filename': str},
-#         outputs={'results': str},
-#         use_latest=True
-#     )(transformed_filename=transformed_filename)
+@task
+def gnb_training_task(transformed_filepath: str) -> str:
+    return DominoJobTask(
+        name='Train GaussianNB classifier',
+        domino_job_config=DominoJobConfig(Command="python exercises/trainer_gnb.py"),
+        inputs={'transformed_filepath': str},
+        outputs={'results': str},
+        use_latest=True
+    )(transformed_filepath=transformed_filepath)
 
-# @task
-# def xgb_training_task(transformed_filename: str) -> str:
-#     return DominoJobTask(
-#         name='Train XGBoost classifier',
-#         domino_job_config=DominoJobConfig(Command="python exercises/trainer_xgb.py"),
-#         inputs={'transformed_filename': str},
-#         outputs={'results': str},
-#         use_latest=True
-#     )(transformed_filename=transformed_filename)
+@task
+def xgb_training_task(transformed_filepath: str) -> str:
+    return DominoJobTask(
+        name='Train XGBoost classifier',
+        domino_job_config=DominoJobConfig(Command="python exercises/trainer_xgb.py"),
+        inputs={'transformed_filepath': str},
+        outputs={'results': str},
+        use_latest=True
+    )(transformed_filepath=transformed_filepath)
 
-# @workflow
-# def credit_card_fraud_detection_workflow() -> str:
-#     transformed_filename = preprocessing_task()
+@workflow
+def credit_card_fraud_detection_workflow() -> str:
+    preprocessing_task = DominoJobTask(
+        name='Feature engineering and preprocessing',
+        domino_job_config=DominoJobConfig(Command="python exercises/c_DataEngineering/data_engineering.py"),
+        inputs={},
+        outputs={'transformed_filepath': str},
+        use_latest=True,
+        cache=False
+    )
     
-#     ada_results = ada_training_task(transformed_filename=transformed_filename)
-#     gnb_results = gnb_training_task(transformed_filename=transformed_filename)
-#     xgb_results = xgb_training_task(transformed_filename=transformed_filename)
+    transformed_filepath = preprocessing_task()
+
+    ada_training_task = DominoJobTask(
+            name='Train AdaBoost classifier',
+            domino_job_config=DominoJobConfig(Command="python exercises/trainer_ada.py"),
+            inputs={'transformed_filepath': str},
+            outputs={'results': str},
+            use_latest=True
+        )
+    gnb_training_task = DominoJobTask(
+            name='Train GaussianNB classifier',
+            domino_job_config=DominoJobConfig(Command="python exercises/trainer_gnb.py"),
+            inputs={'transformed_filepath': str},
+            outputs={'results': str},
+            use_latest=True
+        )
+    xgb_training_task = DominoJobTask(
+            name='Train XGBoost classifier',
+            domino_job_config=DominoJobConfig(Command="python exercises/trainer_xgb.py"),
+            inputs={'transformed_filepath': str},
+            outputs={'results': str},
+            use_latest=True
+        )
+
+    ada_results = ada_training_task(transformed_filepath=transformed_filepath)
+    gnb_results = gnb_training_task(transformed_filepath=transformed_filepath)
+    xgb_results = xgb_training_task(transformed_filepath=transformed_filepath)
     
-#     return ada_results
+    return ada_results
