c9e171a792144bf5738e5011c4b6e71ca52d5fd4
Nick Goble <nick.goble@dominodatalab.com>
2025-07-22 10:40:43 -0700

    Pushed from Domino: https://se-demo.domino.tech/workspace/dmennell/Fraud-Detection-Workshop?executionId=686eeb242cb8c91efbd7ab00

--- exercises/d_TrainingAndEvaluation/workflow.py
+++ exercises/d_TrainingAndEvaluation/workflow.py
@@ -9,6 +9,14 @@ from flytekitplugins.domino.task import DatasetSnapshot
 def credit_card_fraud_detection_workflow() -> str:
     transformed_filename = 'transformed_cc_transactions.csv'
 
+    ada_training_task = DominoJobTask(
+        name='Train AdaBoost classifier',
+        domino_job_config=DominoJobConfig(Command="python exercises/d_TrainingAndEvaluation/trainer_ada.py"),
+        inputs={'transformed_filename': str},
+        outputs={'results': str},
+        use_latest=True
+    )
+
     gnb_training_task = DominoJobTask(
         name='Train GaussianNB classifier',
         domino_job_config=DominoJobConfig(Command="python exercises/d_TrainingAndEvaluation/trainer_gnb.py"),
@@ -17,17 +25,18 @@ def credit_card_fraud_detection_workflow() -> str:
         use_latest=True
     )
 
+    ada_results = ada_training_task(transformed_filename=transformed_filename)
     gnb_results = gnb_training_task(transformed_filename=transformed_filename)
 
     compare_task = DominoJobTask(
         name='Compare training results',
         domino_job_config=DominoJobConfig(Command="python exercises/d_TrainingAndEvaluation/compare_training_results.py"),
-        inputs={'results_json': str},
+        inputs={'ada_results': str, 'gnb_results': str},
         outputs={'comparison': str},
         use_latest=True
     )
 
-    comparison_csv = compare_task(results_json=all_results_json)
+    comparison_csv = compare_task(ada_results=ada_results, gnb_results=gnb_results)
 
     # Return whatever you want Flyte to show as the final output. CSV is convenient.
     return comparison_csv
